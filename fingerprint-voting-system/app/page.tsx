ee4f0af0168482892104f3575795ed3ba4f29809
harshxsingh14 <harsh.singh1403@gmail.com>
2025-12-03 23:26:48 +0530

    modification commit

--- fingerprint-voting-system/app/page.tsx
+++ fingerprint-voting-system/app/page.tsx
@@ -1,65 +1,106 @@
-import Image from "next/image";
+import Hero from "./ui/hero";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="flex min-h-screen flex-col items-center p-24 font-sans">
+     <Hero />
+
+      <div className="mt-32">
+        <p className="text-2xl md:text-5xl text-center leading-loose">
+          Welcome to the next generation of stock market analysis. Our
+          AI-powered application harnesses the power of advanced machine
+          learning algorithms to analyze historical stock data and provide
+          accurate predictions, helping you make informed investment decisions.
+        </p>
+      </div>
+
+      <section id="why-choose-us" className="my-32">
+        <h2 className="text-5xl font-semibold mb-5 text-center border-b border-b-green-300">
+          Why Choose Our AI Chatbot?
+        </h2>
+
+        <ul className="text-xl md:text-3xl ">
+          <li className="list-disc leading-loose">
+            Data-Driven Insights: Leveraging vast amounts of historical stock
+            data, our chatbot delivers data-driven predictions to guide your
+            investment strategy.
+          </li>
+          <li className="list-disc leading-loose">
+            Real-Time Predictions: Get instant predictions and market trends
+            directly from our chatbot, allowing you to stay ahead of the curve.
+          </li>
+          <li className="list-disc leading-loose">
+            User-Friendly Interface: Chat with our intuitive AI to receive
+            easy-to-understand insights, whether you&apos;re a seasoned investor
+            or just starting out.
+          </li>
+          <li className="list-disc leading-loose">
+            Personalized Advice: Tailored recommendations based on your
+            portfolio and investment goals.
+          </li>
+        </ul>
+      </section>
+
+      <section id="how-it-works" className="my-32">
+        <h2 className="text-5xl font-semibold mb-5 text-center border-b border-b-green-300">
+          How It Works
+        </h2>
+
+        <ul className="text-xl md:text-3xl">
+          <li className="list-decimal leading-loose">
+            Connect: Simply start a conversation with our chatbot and input the
+            stocks you&apos;re interested in.
+          </li>
+          <li className="list-decimal leading-loose">
+            Analyze: Our AI analyzes the historical data and applies
+            cutting-edge prediction models to forecast potential stock
+            movements.
+          </li>
+          <li className="list-decimal leading-loose">
+            User-Friendly Interface: Chat with our intuitive AI to receive
+            easy-to-understand insights, whether you&apos;re a seasoned investor
+            or just starting out.
+          </li>
+          <li className="list-decimal leading-loose">
+            Predict: Receive real-time predictions and insights on stock
+            performance, helping you make well-informed decisions.
+          </li>
+        </ul>
+      </section>
+
+      <section id="who-can-benefit" className="my-32">
+        <h2 className="text-5xl font-semibold mb-5 text-center border-b border-b-green-300">
+          Who Can Benefit?
+        </h2>
+
+        <ul className="text-xl md:text-3xl">
+          <li className="list-disc leading-loose">
+            Individual Investors: Whether you&apos;re new to investing or
+            looking to optimize your strategy, our chatbot offers insights to
+            help grow your portfolio.
+          </li>
+          <li className="list-disc leading-loose">
+            Financial Advisors: Enhance your advisory services with AI-driven
+            predictions to provide better guidance to your clients.
+          </li>
+          <li className="list-disc leading-loose">
+            Traders: Stay ahead of market trends and make timely trades with our
+            accurate stock predictions.
+          </li>
+        </ul>
+      </section>
+
+      <div className="mt-32 text-center">
+        <h2 className="text-5xl mb-10 font-semibold">
+          Start Predicting Your Success Today
+        </h2>
+        <p className=" text-2xl md:text-5xl text-center leading-loose">
+          Don&apos;t let the market&apos;s unpredictability hold you back. Join
+          countless others who trust our AI-powered chatbot to navigate the
+          complexities of the stock market. Start chatting now and take control
+          of your financial future.
+        </p>
+      </div>
+    </main>
   );
 }
